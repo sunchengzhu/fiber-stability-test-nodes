e35159f3cf8bb025c0498698bdcd53e401f386c9
sunchengzhu <sunchengzhu@cryptape.com>
2024-12-11 16:37:01 +0800

    update restart.sh

--- restart.sh
+++ restart.sh
@@ -10,3 +10,5 @@ end_node_id=8
 for ((id = $start_node_id; id <= $end_node_id; id++)); do
   RUST_LOG=info ./fnn -c "testnet-fnn/node$id/config.yml" -d "testnet-fnn/node$id" >>"./node$id.log" 2>&1 &
 done
+
+ps aux | grep '[f]nn'
