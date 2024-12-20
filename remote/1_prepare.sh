71538becdaedae2db79a4171c8a55e12c9a9591f
sunchengzhu <sunchengzhu@cryptape.com>
2024-12-20 16:33:28 +0800

    add remote

---
+++ remote/1_prepare.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+cd ..
+pkill fnn
+rm -rf fiber
+
+git clone https://github.com/nervosnetwork/fiber.git
+cd fiber
+cargo build --release
+mkdir tmp
+
+cp target/release/fnn tmp
+cd tmp
+
+# 节点范围定义
+start_node_id=1
+end_node_id=8
+
+# 检查是否已存在 announce_private_addr
+if ! yq eval '.fiber.announce_private_addr' "../config/testnet/config.yml" | grep -q true; then
+  yq eval '.fiber.announce_private_addr = true' -i "../config/testnet/config.yml"
+fi
+yq eval '.fiber.announce_private_addr' "../config/testnet/config.yml"
+
+# 创建目录、复制配置文件并设置密钥，同时更新配置并打印配置情况
+for ((id = $start_node_id; id <= $end_node_id; id++)); do
+  # 创建目录并复制配置文件
+  mkdir -p "testnet-fnn/node$id/ckb"
+  cp "../config/testnet/config.yml" "testnet-fnn/node$id/config.yml"
+  sed -n "${id}p" "../../keys.txt" >"testnet-fnn/node$id/ckb/key"
+
+  # 计算端口号
+  fiber_port=$((8220 + id))
+  rpc_port=$((8230 + id))
+
+  # 根据 id 修改配置文件中的地址
+  if [ "$id" -ge 1 ] && [ "$id" -le 5 ]; then
+    ip="18.167.71.41"
+  elif [ "$id" -eq 6 ]; then
+    ip="43.198.254.225"
+  elif [ "$id" -ge 7 ] && [ "$id" -le 8 ]; then
+    ip="43.199.108.57"
+  fi
+
+  # 更新配置文件中的地址
+  yq eval ".fiber.listening_addr = \"/ip4/$ip/tcp/$fiber_port\"" -i "testnet-fnn/node$id/config.yml"
+  yq eval ".rpc.listening_addr = \"$ip:$rpc_port\"" -i "testnet-fnn/node$id/config.yml"
+
+  # 打印配置情况
+  echo "node$id config.yml"
+  yq eval '.fiber.listening_addr' "testnet-fnn/node$id/config.yml"
+  yq eval '.rpc.listening_addr' "testnet-fnn/node$id/config.yml"
+  echo ""
+done
---
+++ remote/2_run.sh
@@ -0,0 +1,21 @@
+# 获取公共 IP 地址
+IP=$(curl -s ifconfig.me)
+
+# 根据 IP 地址决定要删除的节点
+case "$IP" in
+  "18.167.71.41")
+    # 删除除 node1 到 node5 以外的节点 (即删除 node6, node7, node8)
+    rm -rf ../fiber/tmp/testnet-fnn/node{6..8}
+    ;;
+  "43.198.254.225")
+    # 删除除 node6 外的节点 (即删除 node1 到 node5 和 node7 到 node8)
+    rm -rf ../fiber/tmp/testnet-fnn/node{1..5} ../fiber/tmp/testnet-fnn/node{7..8}
+    ;;
+  "43.199.108.57")
+    # 删除除 node7 和 node8 外的节点 (即删除 node1 到 node6)
+    rm -rf ../fiber/tmp/testnet-fnn/node{1..6}
+    ;;
+  *)
+    echo "不支持的 IP 地址: $IP"
+    ;;
+esac
