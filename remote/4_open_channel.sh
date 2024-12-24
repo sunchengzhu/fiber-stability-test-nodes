343eeaa1a46068e58801866a7a2ff8a84c990684
sunchengzhu <sunchengzhu@cryptape.com>
2024-12-24 19:31:49 +0800

    add 4_open_channel.sh

---
+++ remote/4_open_channel.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+check_channels_ready() {
+  local port=$1
+  local peer_id=$2
+  local start_time=$(date +%s)
+  local timeout=180
+
+  while true; do
+    # 直接在 curl 请求中构造 JSON 数据
+    local states=$(curl -sS --location "http://127.0.0.1:$port" \
+      --header "Content-Type: application/json" \
+      -d "{
+        \"id\": \"1\",
+        \"jsonrpc\": \"2.0\",
+        \"method\": \"list_channels\",
+        \"params\": [{
+          \"peer_id\": \"$peer_id\"
+        }]
+      }" | jq -r '.result.channels[].state.state_name')
+
+    # 如果 states 为空，则继续等待
+    if [[ -z "$states" ]]; then
+      echo "Port $port states is empty, retrying..."
+    else
+      echo "Port $port states: $states"
+
+      # 检查是否所有状态都为 CHANNEL_READY
+      local all_ready=true
+      for state in $states; do
+        if [[ "$state" != "CHANNEL_READY" ]]; then
+          all_ready=false
+          break
+        fi
+      done
+
+      # 如果所有状态都为 CHANNEL_READY，则退出
+      if [[ "$all_ready" == "true" ]]; then
+        local current_time=$(date +%s)
+        local elapsed_time=$((current_time - start_time))
+        echo "所有通道都已准备就绪，总耗时：${elapsed_time}秒。"
+        return
+      fi
+    fi
+
+    # 计算已经过的时间
+    local current_time=$(date +%s)
+    local elapsed_time=$((current_time - start_time))
+
+    # 超过时间限制则退出
+    if [[ "$elapsed_time" -ge "$timeout" ]]; then
+      echo "超时：180秒内未所有通道都准备就绪。"
+      return
+    fi
+
+    # 等待5秒再次检查
+    sleep 5
+  done
+}
+
+PORTS=($(seq 8231 8238))
+peer_ids=()
+
+for idx in "${!PORTS[@]}"; do
+  PORT=${PORTS[idx]}
+  if [ "$idx" -lt 5 ]; then
+    ip="18.167.71.41"
+  elif [ "$idx" -eq 5 ]; then
+    ip="43.198.254.225"
+  elif [ "$idx" -ge 6 ]; then
+    ip="43.199.108.57"
+  fi
+
+  response=$(curl -s -X POST http://"$ip":"$PORT" \
+    -H "Content-Type: application/json" \
+    -d "$(printf '{
+                 "id": %d,
+                 "jsonrpc": "2.0",
+                 "method": "node_info",
+                 "params": []
+             }' "$id")")
+  if [ $? -eq 0 ]; then
+    peer_id=$(echo "$response" | jq -r '.result.peer_id' | sed "s/0.0.0.0/$ip/")
+    peer_ids+=("$peer_id")
+  else
+    echo "Query to port $PORT failed."
+  fi
+done
+
+f_peer_id="${peer_ids[5]}"
+g_peer_id="${peer_ids[6]}"
+
+open_channel_f_json_data=$(
+  cat <<EOF
+{
+  "id": "%s",
+  "jsonrpc": "2.0",
+  "method": "open_channel",
+  "params": [
+    {
+      "peer_id": "$f_peer_id",
+      "funding_amount": "0x174876e800",
+      "public": true
+    }
+  ]
+}
+EOF
+)
+
+open_channel_g_json_data=$(
+  cat <<EOF
+{
+  "id": "%s",
+  "jsonrpc": "2.0",
+  "method": "open_channel",
+  "params": [
+    {
+      "peer_id": "$g_peer_id",
+      "funding_amount": "0x1bf08eb000",
+      "public": true
+    }
+  ]
+}
+EOF
+)
+
+current_ip=$(curl -s ifconfig.me)
+
+if [ "$current_ip" == "18.167.71.41" ]; then
+  for i in 0 1 2 3 4; do
+    port="${PORTS[i]}"
+    json_data=$(printf "$open_channel_f_json_data" "$port")
+    curl --location "http://$current_ip:$port" --header "Content-Type: application/json" --data "$json_data"
+    echo ""
+    check_channels_ready "$port" "$f_peer_id"
+  done
+elif [ "$current_ip" == "43.198.254.225" ]; then
+  port="${PORTS[5]}"
+  json_data=$(printf "$open_channel_g_json_data" "$port")
+  curl -sS --location "http://$current_ip:$port" --header "Content-Type: application/json" --data "$json_data"
+  echo ""
+  check_channels_ready "$port" "$g_peer_id"
+elif [ "$current_ip" == "43.199.108.57" ]; then
+  port1="${PORTS[6]}"
+  json_data1=$(printf "$open_channel_f_json_data" "$port1")
+  curl -sS --location "http://$current_ip:$port1" --header "Content-Type: application/json" --data "$json_data1"
+  echo ""
+  check_channels_ready "$port1" "$f_peer_id"
+
+  port2="${PORTS[7]}"
+  json_data2=$(printf "$open_channel_g_json_data" "$port2")
+  curl -sS --location "http://$current_ip:$port2" --header "Content-Type: application/json" --data "$json_data2"
+  check_channels_ready "$port2" "$g_peer_id"
+fi
