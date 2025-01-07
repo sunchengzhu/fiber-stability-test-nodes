042e24a458405b518e55dfb4d536f106149c154d
sunchengzhu <sunchengzhu@cryptape.com>
2025-01-07 10:51:25 +0800

    use tmp repo

--- remote/1_prepare.sh
+++ remote/1_prepare.sh
@@ -7,8 +7,9 @@ pkill fnn
 
 if [ "$MODE" -eq 1 ]; then
   rm -rf fiber
-  git clone https://github.com/nervosnetwork/fiber.git
+  git clone https://github.com/chenyukang/fiber.git
   cd fiber
+  git checkout yukang-debug-send-each-other
   cargo build --release
 else
   cd fiber
