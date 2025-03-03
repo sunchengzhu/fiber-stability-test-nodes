fbed752631b77e11ff7558b9f0aafe495424f56f
sunchengzhu <sunchengzhu@cryptape.com>
2025-03-03 19:52:20 +0800

    update rollback.sh

--- remote/rollback.sh
+++ remote/rollback.sh
@@ -5,15 +5,18 @@ cp /home/ckb/scz/0_4_0/fnn ../fiber/tmp/fnn
 #cp /home/ckb/scz/2858151/fnn ../fiber/tmp/fnn
 ../fiber/tmp/fnn --version
 
-used_store_name="2025-03-03_17_store"
+#used_store_name="2025-03-03_17_store"
 bak_store_name="2858151_store"
 
 cd ../fiber/tmp
 for dir in $(ls -d ./testnet-fnn/node*); do
   node_id=$(basename "$dir")
   echo "$node_id"
-  mv "./testnet-fnn/$node_id/fiber/store" "./testnet-fnn/$node_id/fiber/$bak_store_name"
-  mv "./testnet-fnn/$node_id/fiber/$used_store_name" "./testnet-fnn/$node_id/fiber/store"
+  rm -rf "./testnet-fnn/$node_id/fiber/store"
+  cp -r "./testnet-fnn/$node_id/fiber/$bak_store_name" "./testnet-fnn/$node_id/fiber/store"
+  ./fnn-migrate -p "testnet-fnn/$node_id/fiber/"
+#  mv "./testnet-fnn/$node_id/fiber/store" "./testnet-fnn/$node_id/fiber/$bak_store_name"
+#  mv "./testnet-fnn/$node_id/fiber/$used_store_name" "./testnet-fnn/$node_id/fiber/store"
   RUST_LOG=info ./fnn -c "$dir/config.yml" -d "$dir" >"./testnet-fnn/$node_id/$node_id.log" 2>&1 &
   sleep 5
   head -n 1 "./testnet-fnn/$node_id/$node_id.log"
