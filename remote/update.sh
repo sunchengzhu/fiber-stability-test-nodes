38747ebdd5de647d5da69485c6b1a88d4593cbc4
sunchengzhu <sunchengzhu@cryptape.com>
2025-03-04 16:08:14 +0800

    update update.sh

--- remote/update.sh
+++ remote/update.sh
@@ -3,7 +3,7 @@
 pkill fnn
 
 rm -f ../fiber/tmp/fnn
-cp /home/ckb/scz/0_4_0/fnn ../fiber/tmp/fnn
+cp /home/ckb/scz/version/fnn ../fiber/tmp/fnn
 ../fiber/tmp/fnn --version
 
 config_name="0_4_0-config.yml"
@@ -13,7 +13,7 @@ for dir in $(ls -d ./testnet-fnn/node*); do
   node_id=$(basename "$dir")
   echo "$node_id"
   rm -f "./testnet-fnn/$node_id/config.yml"
-  cp "./testnet-fnn/$node_id/$config_name" "./testnet-fnn/$node_id/config.yml"
+  cp "/home/ckb/scz/version/$node_id/$config_name" "./testnet-fnn/$node_id/config.yml"
   ./fnn-migrate -p "testnet-fnn/$node_id/fiber/store"
   RUST_LOG=info ./fnn -c "$dir/config.yml" -d "$dir" >"./testnet-fnn/$node_id/$node_id.log" 2>&1 &
   sleep 5
