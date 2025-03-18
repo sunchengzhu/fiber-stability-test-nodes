c0f6bd429e6214c11243ffdb01f8e503c1ee663c
sunchengzhu <sunchengzhu@cryptape.com>
2025-03-18 16:40:28 +0800

    update 2_run.sh

--- local/2_run.sh
+++ local/2_run.sh
@@ -10,6 +10,7 @@ end_node_id=3
 for ((id = $start_node_id; id <= $end_node_id; id++)); do
   RUST_LOG=info ./fnn -c "testnet-fnn/node$id/config.yml" -d "testnet-fnn/node$id" >>"./node$id.log" 2>&1 &
   sleep 3
+  ./fnn --version
   head -n 1 "./node$id.log"
 done
 
