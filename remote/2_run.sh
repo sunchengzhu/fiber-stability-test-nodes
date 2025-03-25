f9fc4105209c7f19d61f0c21a51dd60226582f7e
sunchengzhu <sunchengzhu@cryptape.com>
2025-03-25 16:46:00 +0800

    rename fnn_session

--- remote/2_run.sh
+++ remote/2_run.sh
@@ -29,7 +29,7 @@ ls ../fiber/testnet-fnn
 cd ../fiber
 for dir in $(ls -d ./testnet-fnn/node*); do
   node_id=$(basename "$dir")
-  tmux new-session -d -s fnn_session "RUST_LOG=info ./fnn -c \"$dir/config.yml\" -d \"$dir\" > \"./testnet-fnn/$node_id/$node_id.log\" 2>&1"
+  tmux new-session -d -s fnn_session_${node_id} "RUST_LOG=info ./fnn -c \"$dir/config.yml\" -d \"$dir\" > \"./testnet-fnn/$node_id/$node_id.log\" 2>&1"
   sleep 3
   ./fnn --version
   head -n 1 "./testnet-fnn/$node_id/$node_id.log"
