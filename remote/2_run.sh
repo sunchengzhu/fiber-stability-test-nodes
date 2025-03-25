29d32bcc87ea1a1a97dbb8b4c08f00fd6988c4f4
sunchengzhu <sunchengzhu@cryptape.com>
2025-03-25 16:17:06 +0800

    use nohup

--- remote/2_run.sh
+++ remote/2_run.sh
@@ -29,7 +29,7 @@ ls ../fiber/testnet-fnn
 cd ../fiber
 for dir in $(ls -d ./testnet-fnn/node*); do
   node_id=$(basename "$dir")
-  RUST_LOG=info ./fnn -c "$dir/config.yml" -d "$dir" >"./testnet-fnn/$node_id/$node_id.log" 2>&1 &
+  nohup sh -c 'RUST_LOG=info ./fnn -c "'$dir'/config.yml" -d "'$dir'" >"./testnet-fnn/'$node_id'/'$node_id'.log" 2>&1' &
   sleep 3
   ./fnn --version
   head -n 1 "./testnet-fnn/$node_id/$node_id.log"
