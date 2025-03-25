50d2b933437a0b39ed8389e1949a34e77f84ef19
sunchengzhu <sunchengzhu@cryptape.com>
2025-03-25 17:32:41 +0800

    update 3_connect_peer.sh

--- remote/3_connect_peer.sh
+++ remote/3_connect_peer.sh
@@ -65,23 +65,23 @@ if [ "$current_ip" == "18.167.71.41" ]; then
   for i in 0 1 2 3 4; do
     port="${PORTS[i]}"
     json_data=$(printf "$connect_peer_f_json_data" "$port")
-    curl --location "http://172.31.23.160:$port" --header "Content-Type: application/json" --data "$json_data"
+    curl -s --location "http://172.31.23.160:$port" --header "Content-Type: application/json" --data "$json_data"
     echo ""
   done
 elif [ "$current_ip" == "43.198.254.225" ]; then
   port="${PORTS[5]}"
   json_data=$(printf "$connect_peer_g_json_data" "$port")
-  curl --location "http://172.31.28.209:$port" --header "Content-Type: application/json" --data "$json_data"
+  curl -s --location "http://172.31.28.209:$port" --header "Content-Type: application/json" --data "$json_data"
   echo ""
 elif [ "$current_ip" == "43.199.108.57" ]; then
   # g → f
   port1="${PORTS[6]}"
   json_data1=$(printf "$connect_peer_f_json_data" "$port1")
-  curl --location "http://172.31.16.223:$port1" --header "Content-Type: application/json" --data "$json_data1"
+  curl -s --location "http://172.31.16.223:$port1" --header "Content-Type: application/json" --data "$json_data1"
   echo ""
   # h → g
   port2="${PORTS[7]}"
   json_data2=$(printf "$connect_peer_g_json_data" "$port2")
-  curl --location "http://172.31.16.223:$port2" --header "Content-Type: application/json" --data "$json_data2"
+  curl -s --location "http://172.31.16.223:$port2" --header "Content-Type: application/json" --data "$json_data2"
   echo ""
 fi
