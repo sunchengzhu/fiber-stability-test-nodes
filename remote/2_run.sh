4b9fb240d3aa5774630fa8cb4c2c16b9ce472851
sunchengzhu <sunchengzhu@cryptape.com>
2025-04-29 17:22:40 +0800

    debug

--- remote/2_run.sh
+++ remote/2_run.sh
@@ -9,7 +9,8 @@ IP=$(curl -s ifconfig.me)
 case "$IP" in
 "18.167.71.41")
   # 删除除 node1 到 node5 以外的节点 (即删除 node6, node7, node8)
-  rm -rf ../fiber/testnet-fnn/node{6..8}
+#  rm -rf ../fiber/testnet-fnn/node{6..8}
+   rm -rf ../fiber/testnet-nn/node{2..8}
   ;;
 "43.198.254.225")
   # 删除除 node6 外的节点 (即删除 node1 到 node5 和 node7 到 node8)
@@ -17,7 +18,8 @@ case "$IP" in
   ;;
 "43.199.108.57")
   # 删除除 node7 和 node8 外的节点 (即删除 node1 到 node6)
-  rm -rf ../fiber/testnet-fnn/node{1..6}
+#  rm -rf ../fiber/testnet-fnn/node{1..6}
+  rm -rf ../fiber/testnet-fnn/node{1..6} ../fiber/testnet-fnn/node8
   ;;
 *)
   echo "不支持的 IP 地址: $IP"
