14068331533fcbf24e4109e0fadac33f0d567510
sunchengzhu <sunchengzhu@cryptape.com>
2025-04-30 19:33:21 +0800

    update 5_shutdown_channel.sh

--- remote/5_shutdown_channel.sh
+++ remote/5_shutdown_channel.sh
@@ -92,13 +92,26 @@ if [ "$current_ip" == "18.167.71.41" ]; then
   for i in 0; do
     port="${PORTS[i]}"
     json_data=$(printf "$list_channels_f_json_data" "$port")
-    channel_id=$(curl -sS --location "http://$current_ip:$port" --header "Content-Type: application/json" --data "$json_data" | jq -r '.result.channels[0].channel_id')
-    echo "$channel_id"
-    echo ""
-    args=$(sed -n "$((i + 1))p" ../args.txt)
-    shutdown_channel_json_data=$(printf "$shutdown_channel_json_template" "$port" "$channel_id" "$args")
-    if [[ "$channel_id" != "null" && -n "$channel_id" ]]; then
-      curl -sS --location "http://$current_ip:$port" --header "Content-Type: application/json" --data "$shutdown_channel_json_data"
+
+    channel_ids=$(curl -sS --location "http://$current_ip:$port" \
+      --header "Content-Type: application/json" \
+      --data "$json_data" | jq -r '.result.channels[].channel_id')
+
+    if [[ -n "$channel_ids" ]]; then
+      args=$(sed -n "$((5 + 1))p" ../args.txt)
+
+      for channel_id in $channel_ids; do
+        if [[ "$channel_id" != "null" && -n "$channel_id" ]]; then
+          echo "$channel_id"
+          shutdown_channel_json_data=$(printf "$shutdown_channel_json_template" "$port" "$channel_id" "$args")
+          curl -sS --location "http://$current_ip:$port" \
+            --header "Content-Type: application/json" \
+            --data "$shutdown_channel_json_data"
+        fi
+        echo ""
+      done
+    else
+      echo "No channels found to shutdown."
     fi
   done
 elif [ "$current_ip" == "43.198.254.225" ]; then
