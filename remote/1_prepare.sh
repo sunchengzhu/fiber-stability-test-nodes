5ab9abfd476422a4be2515ea7e9a233a91fcf34c
sunchengzhu <sunchengzhu@cryptape.com>
2025-06-09 22:04:40 +0800

    bugfix

--- remote/1_prepare.sh
+++ remote/1_prepare.sh
@@ -37,10 +37,10 @@ for ((id = $start_node_id; id <= $end_node_id; id++)); do
   rpc_port=$((8230 + id))
 
   if [ "$id" -eq 1 ]; then
-    yq -i '.fiber."standalone-watchtower-rpc-url" = "16.162.235.63:8227"' "testnet-fnn/node${id}/config.yml"
+    yq -i '.fiber."standalone_watchtower_rpc_url" = "16.162.235.63:8227"' "testnet-fnn/node${id}/config.yml"
     yq -i '.fiber.disable_built_in_watchtower = true' "testnet-fnn/node${id}/config.yml"
 #    yq -i '.rpc.enabled_modules = ["watchtower"]' "testnet-fnn/node${id}/config.yml"
-    grep -E 'standalone-watchtower-rpc-url|disable_built_in_watchtower' "testnet-fnn/node${id}/config.yml"
+    grep -E 'standalone_watchtower_rpc_url|disable_built_in_watchtower' "testnet-fnn/node${id}/config.yml"
 #    grep -A 2 'enabled_modules' "testnet-fnn/node${id}/config.yml"
   fi
 
