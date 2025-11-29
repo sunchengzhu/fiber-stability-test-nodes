453ab3a6e69f3fe29cd7ce8aa2d8a2a05dac5c69
sunchengzhu <sunchengzhu@cryptape.com>
2025-11-29 16:21:15 +0800

    sleep 30s for Multi channels

--- remote/4_open_channel.sh
+++ remote/4_open_channel.sh
@@ -146,7 +146,7 @@ if [ "$current_ip" == "18.167.71.41" ]; then
 	for ((i = 1; i <= OPEN_CHANNEL_COUNT; i++)); do
 		curl -sS --location "http://172.31.23.160:$port" --header "Content-Type: application/json" --data "$open_channel_f_json_data"
 		echo ""
-		sleep 10
+		sleep 30
 	done
 	check_channels_ready "$port" "$f_peer_id" "172.31.23.160"
 elif [ "$current_ip" == "43.198.254.225" ]; then
@@ -154,7 +154,7 @@ elif [ "$current_ip" == "43.198.254.225" ]; then
 	for ((i = 1; i <= OPEN_CHANNEL_COUNT; i++)); do
 		curl -sS --location "http://172.31.28.209:$port" --header "Content-Type: application/json" --data "$open_channel_g_json_data"
 		echo ""
-		sleep 10
+		sleep 30
 	done
 	check_channels_ready "$port" "$g_peer_id" "172.31.28.209"
 fi
